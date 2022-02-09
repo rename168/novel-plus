1a08e8fe1b8033c08e8e343749469392f110f65b
mannex <mannex@111.com>
2022-02-09 09:23:10 +0800

    Update HttpUtil.java

--- novel-common/src/main/java/com/java2nb/novel/core/utils/HttpUtil.java
+++ novel-common/src/main/java/com/java2nb/novel/core/utils/HttpUtil.java
@@ -1,5 +1,11 @@
 package com.java2nb.novel.core.utils;
 
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 
@@ -10,6 +16,7 @@ public class HttpUtil {
 
     private static RestTemplate restTemplate = RestTemplateUtil.getInstance("utf-8");
 
+    private final static Logger logger = LoggerFactory.getLogger(HttpUtil.class);
 
     public static String getByHttpClient(String url) {
         try {
@@ -25,13 +32,25 @@ public class HttpUtil {
             return null;
         }
     }
+    private static Map<String, Long > urlMap = new ConcurrentHashMap<>();
 
+    private static long reqCount = 0 ;
     public static String getByHttpClientWithChrome(String url) {
         try {
+            Long lasttime = urlMap.get(url) ;
+            if( lasttime !=null  && System.currentTimeMillis()-lasttime<3000 ){
+                logger.error(String.format( " 已经正在请求 reqCount %d, url: %s  time:%d" ,reqCount++ , url , System.currentTimeMillis()-lasttime)) ;
+                // Throwable t =  new Throwable(url) ;
+                // t.printStackTrace();
+                // return null ;
+            }else {
+                logger.info(String.format( "getByHttpClientWithChrome reqCount %d,  url: %s ",reqCount++ , url)) ;
+            }
 
             HttpHeaders headers = new HttpHeaders();
             headers.add("user-agent","Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.67 Safari/537.36");
             HttpEntity<String> requestEntity = new HttpEntity<>(null, headers);
+            urlMap.put(url, System.currentTimeMillis() );
             ResponseEntity<String> forEntity = restTemplate.exchange(url.toString(), HttpMethod.GET, requestEntity, String.class);
 
             if (forEntity.getStatusCode() == HttpStatus.OK) {
@@ -40,6 +59,8 @@ public class HttpUtil {
                 return null;
             }
         } catch (Exception e) {
+            logger.error(String.format( " 请求失败 url: %s  Message:%s" , url ,e.getMessage())) ;
+
             e.printStackTrace();
             return null;
         }
